dcb83cd45c2971bdfd33c9aa7aad82ac48739dc1
Dzmitry Aksiantsiuk <dzmitry.aksiantsiuk@gmail.com>
2021-02-05 23:14:10 +0300

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import './App.scss';
 import Input from './components/Input';
 import CardList from './components/CardList';
 import ButtonList from './components/ButtonList';
+import Footer from './components/Footer';
 
 const App = () => (
   <div>
     <Input />
     <CardList />
     <ButtonList />
+    <Footer />
   </div>
 );
 
